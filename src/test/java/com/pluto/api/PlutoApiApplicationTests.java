fb86944af74c31446b95452eb6606e4d3aebad91
sebastian-reyes <sebastianreyes2105@gmail.com>
2023-10-06 01:04:49 -0500

    first commit

---
+++ src/test/java/com/pluto/api/PlutoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluto.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlutoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
